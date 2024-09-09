0bab47f0a838b1dbbf8167b16b1cea35943dba9f
fighterrajput <ankitsinghtushar@gmail.com>
2024-09-09 14:40:01 +0530

    done

---
+++ spring-mvc/src/main/java/com/rays/ctl/WelcomeCtl.java
@@ -0,0 +1,29 @@
+package com.rays.ctl;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "Welcome")
+public class WelcomeCtl {
+
+	@GetMapping
+	public String display() {
+		System.out.println("display");
+		return "Welcome";
+	}
+
+	@GetMapping("first")
+	public String display1() {
+		System.out.println("display1");
+		return "Welcome";
+	}
+
+	@GetMapping("second")
+	public String display2() {
+		System.out.println("display2");
+		return "Welcome";
+	}
+
+}
